eb5971e5d56ba783894baf690ddb3640affd999e
Priscilla Alfaro <priscilla@comfyapp.com>
2018-02-28 12:58:46 -0800

    Add logic to identify the mode between air, light and rooms and show colors accordingly

--- led_viz.js
+++ led_viz.js
@@ -24,9 +24,51 @@ function draw() {
 
   // Draw location temperature colors
   for (var pixel = 0; pixel < NUM_PIXELS; pixel++) {
-    var temp = locations[pixel].temp;
-    let hue =((temp -65) *0.02) +0.65;
-    client.setPixel(pixel, ...OPC.hsv(hue, 1, 1));
+    let location = locations[pixel];
+    let mode = location.mode;
+    
+    let hue, saturation = 1, brightness = 1;
+    
+    switch(mode){
+      case 'air':
+        let temp = location.temp;
+        // If temp not available display orange color to alert
+        if(temp===null){
+          hue = 0.10;
+        }
+        else{
+          hue =((temp -65) *0.02) +0.65;
+        }
+        break;
+      case 'light':
+        let levelPercent = location.levelPercent;
+        brightness = levelPercent/100;
+        hue = 0.15; //color yellow
+        break;
+      case 'room':
+        let reservedStatus =location.reservedStatus;
+        if (reservedStatus === 'reserved'){
+          hue = 0.45; //dim cyan
+          saturation = 0.4;
+          brightness = 0.4;
+        }
+        else if(reservedStatus === 'occupied'){
+          hue = 0.45; //cyan
+        }
+        else if (reservedStatus === 'available'){
+          hue = 0.35; //green
+        }
+        else{
+          //grey
+          hue = 0.35; 
+          saturation = 0;
+          brightness = 0.4;
+        }
+        break;
+      default:
+        brightness = 0;
+    }
+    client.setPixel(pixel, ...OPC.hsv(hue, saturation, brightness));
   }
 
   // Draw blasts if any
